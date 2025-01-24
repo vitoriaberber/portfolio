04412356343d9154835620ee03b95a3b9c1e4537
Vitória Berber <vitoriaberberdev@gmail.com>
2025-01-24 18:16:37 -0300

    botao de modo terminado

--- js/main.js
+++ js/main.js
@@ -33,10 +33,34 @@ window.addEventListener('scroll', () => {
 modo.addEventListener('click', () => {
     const btnModo = document.querySelector('.modo__botao');
     const body = document.getElementById('body');
+    const fundo = document.querySelector('.principal__sessao-principal');
+    const fundoContainer = document.querySelectorAll('.fundo');
+    const fundoTexto = document.querySelectorAll('.texto__modo');
+    const fundoBotao = document.querySelector('.fundo-botao');
+    const imagemMenu = document.querySelector('.container__imagem-menu');
+
     btnModo.classList.toggle('claro');
     if(btnModo.classList.contains('claro')){
-        body.style.backgroundColor = 'white';
+        body.style.backgroundColor = '#E0E0E0';
+        fundo.classList.add('invertido');
+        fundoContainer.forEach(item => {
+            item.style.backgroundColor = 'white';
+        });
+        fundoTexto.forEach(item => {
+            item.style.color = 'black';
+        });
+        fundoBotao.style.backgroundColor = 'black';
+        imagemMenu.setAttribute('src', 'assets/menu-preto.svg');
     } else {
         body.style.backgroundColor = '';
+        fundo.classList.remove('invertido');
+        fundoContainer.forEach(item => {
+            item.style.backgroundColor = '';
+        });
+        fundoTexto.forEach(item => {
+            item.style.color = '';
+        });
+        fundoBotao.style.backgroundColor = '';
+        imagemMenu.setAttribute('src', 'assets/menu-branco.svg');
     }
 });
